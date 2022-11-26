85756ab319e87d1f43e7705b5a556cf1dbaf4cd5
Ryo Somatani <ryo_somatani@outlook.com>
2022-11-25 21:53:52 -0800

    add: add file to register PWA

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { WidgetsDataProvider } from './contexts/WidgetsContext';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,3 +11,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </WidgetsDataProvider>
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
